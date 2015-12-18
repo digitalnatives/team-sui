beabfe0baa4194fe260f1b285782e33bb249c2ad
Iuri Fernandes <iurifq@gmail.com>
2015-12-18 08:13:44 -0300

    Implement basic handshake server logic
    Still missing the game start

---
+++ sui_server/web/channels/game_channel.ex
@@ -0,0 +1,33 @@
+defmodule SuiServer.GameChannel do
+  use Phoenix.Channel
+  require Logger
+
+  def join("game:lobby", _message, socket) do
+    Process.flag(:trap_exit, true)
+    send(self, :new_player)
+
+    {:ok, socket}
+  end
+
+  def join("game:" <> game_id, _message, socket) do
+    Process.flag(:trap_exit, true)
+    send(self, {:game_connect, %{game_id: game_id}})
+
+    {:ok, socket}
+  end
+
+  def handle_info(:new_player, socket) do
+    broadcast! socket, "player:entered", %{name: socket.assigns.username}
+    {:noreply, socket}
+  end
+
+  def handle_info({:game_connect, msg}, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_in("new:game", msg, socket) do
+    game_id = SecureRandom.urlsafe_base64(4)
+    push socket, "new:msg", %{game_id: game_id }
+    {:reply, :ok, assign(socket, :game_id, game_id)}
+  end
+end
